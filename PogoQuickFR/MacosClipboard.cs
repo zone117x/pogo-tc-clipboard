756a1b31a0ac282a60670f2a39769f08f60d36f1
Matthew Little <zone117x@gmail.com>
2020-12-19 13:01:40 +0100

    initial commit

---
+++ PogoQuickFR/MacosClipboard.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+static class OsxClipboard
+{
+    static IntPtr nsString = objc_getClass("NSString");
+    static IntPtr nsPasteboard = objc_getClass("NSPasteboard");
+    static IntPtr nsStringPboardType;
+    static IntPtr generalPasteboard;
+    static IntPtr initWithUtf8Register = sel_registerName("initWithUTF8String:");
+    static IntPtr allocRegister = sel_registerName("alloc");
+    static IntPtr stringForTypeRegister = sel_registerName("stringForType:");
+    static IntPtr utf8Register = sel_registerName("UTF8String");
+    static IntPtr generalPasteboardRegister = sel_registerName("generalPasteboard");
+    static IntPtr changeCountRegister = sel_registerName("changeCount");
+        
+
+    static OsxClipboard()
+    {
+        nsStringPboardType = objc_msgSend(objc_msgSend(nsString, allocRegister), initWithUtf8Register, "NSStringPboardType");
+        generalPasteboard = objc_msgSend(nsPasteboard, generalPasteboardRegister);
+    }
+
+    public static long GetChangeCount()
+    {
+        var ptr = objc_msgSend(generalPasteboard, changeCountRegister);
+        return ptr.ToInt64();
+    }
+
+    public static string GetText()
+    {
+        var ptr = objc_msgSend(generalPasteboard, stringForTypeRegister, nsStringPboardType);
+        var charArray = objc_msgSend(ptr, utf8Register);
+        return Marshal.PtrToStringAnsi(charArray);
+    }
+
+    [DllImport("/System/Library/Frameworks/AppKit.framework/AppKit")]
+    static extern IntPtr objc_getClass(string className);
+
+    [DllImport("/System/Library/Frameworks/AppKit.framework/AppKit")]
+    static extern IntPtr objc_msgSend(IntPtr receiver, IntPtr selector);
+
+    [DllImport("/System/Library/Frameworks/AppKit.framework/AppKit")]
+    static extern IntPtr objc_msgSend(IntPtr receiver, IntPtr selector, string arg1);
+
+    [DllImport("/System/Library/Frameworks/AppKit.framework/AppKit")]
+    static extern IntPtr objc_msgSend(IntPtr receiver, IntPtr selector, IntPtr arg1);
+
+    [DllImport("/System/Library/Frameworks/AppKit.framework/AppKit")]
+    static extern IntPtr sel_registerName(string selectorName);
+}
---
+++ PogoQuickFR/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+const string PUSHBULLET_ACCESS_TOKEN = "<YOUR TOKEN HERE>";
+const string PUSHBULLET_ENDPOINT = "https://api.pushbullet.com/v2/ephemerals";
+Regex FRIEND_CODE_REGEX = new Regex(@"(?:\d[ ]*){12}", RegexOptions.Compiled);
+HttpClient client = new HttpClient();
+long LastClipboardChangeCount = OsxClipboard.GetChangeCount();
+
+while (true)
+{
+    if (TryGetNewClipboardContent(out var text) && TryFormatFriendCode(text, out var friendCode))
+    {
+        Console.WriteLine($"Sending friend code to clipboard: {friendCode}");
+        var content = JsonContent.Create(new
+        {
+            type = "push",
+            push = new
+            {
+                body = friendCode,
+                source_device_iden = "ujpah72o0sjAoRtnM0jc",
+                source_user_iden = "ujpah72o0",
+                type = "clip"
+            },
+        });
+        content.Headers.Add("Access-Token", PUSHBULLET_ACCESS_TOKEN);
+        try
+        {
+            var response = await client.PostAsync(PUSHBULLET_ENDPOINT, content);
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine($"Error ${response.StatusCode}");
+                string responseBody = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(responseBody);
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Message :{e.Message}");
+        }
+    }
+    Thread.Sleep(250);
+}
+
+bool TryGetNewClipboardContent(out string content)
+{
+    var changeCount = OsxClipboard.GetChangeCount();
+    var clibboardUpdated = LastClipboardChangeCount != changeCount;
+    LastClipboardChangeCount = changeCount;
+    if (clibboardUpdated)
+    {
+        var text = OsxClipboard.GetText();
+        if (!string.IsNullOrWhiteSpace(text))
+        {
+            content = text;
+            return true;
+        }
+    }
+    content = null;
+    return false;
+}
+
+bool TryFormatFriendCode(string friendCode, out string formattedFriendCode)
+{
+    var match = FRIEND_CODE_REGEX.Match(friendCode);
+    if (!match.Success)
+    {
+        formattedFriendCode = null;
+        return false;
+    }
+    formattedFriendCode = Regex.Replace(match.Value, @"\s+", string.Empty);
+    return true;
+}
